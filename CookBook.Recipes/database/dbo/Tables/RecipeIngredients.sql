95c90c40045975cedb6568a326b9bc5be89e1f80
Laura Kolčavová <53749720+laura-kolcavova@users.noreply.github.com>
2024-01-03 00:25:29 +0100

    feat: Add database project

---
+++ CookBook.Recipes/database/dbo/Tables/RecipeIngredients.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[RecipeIngredients]
+(
+    [Id]                TINYINT IDENTITY(1, 1) NOT NULL,
+    [RecipeId]          BIGINT NOT NULL,
+    [Note]              NVARCHAR(256) NOT NULL,
+    [OrderIndex]        TINYINT NOT NULL DEFAULT 0,
+    [DateCreatedAt]     DATETIMEOFFSET DEFAULT SYSDATETIMEOFFSET(),
+    [DateUpdatedAt]     DATETIMEOFFSET,
+
+    CONSTRAINT [PK_RecipeIngredients] PRIMARY KEY CLUSTERED ([Id] ASC),
+
+    CONSTRAINT [FK_RecipeIngredients_Recipes] FOREIGN KEY ([RecipeId])
+    REFERENCES [dbo].[Recipes] ([Id]) ON DELETE CASCADE
+)
