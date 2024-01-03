4c935bd56a146d8a33fd9ca04ce405d29625dea6
Laura Kolčavová <laura.kolcavova@notino.com>
2024-01-03 01:12:32 +0100

    feat: Add Api project

---
+++ CookBook.Recipes/src/CookBook.Recipes.Api/Program.cs
@@ -0,0 +1,98 @@
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host
+    .UseDefaultServiceProvider((context, options) =>
+    {
+        options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+        options.ValidateOnBuild = context.HostingEnvironment.IsDevelopment();
+    });
+
+// Add services to the container.
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+var cookBookRecipesConnectionString = configuration
+    .GetSqlConnectionString(ConfigurationConstants.CookBookRecipesConnectionStringSectionName);
+
+services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options => builder.Configuration.GetSection(nameof(JwtBearerOptions)).Bind(options));
+
+services
+    .AddOptions();
+
+services
+    .AddHealthChecks()
+    .AddSqlServer(cookBookRecipesConnectionString, name: "CookBookRecipes_DB", tags: new[] { "readiness" });
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerExamplesFromAssemblyOf<Program>()
+    .AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo { Title = builder.Environment.ApplicationName, Version = "v1" });
+        options.ExampleFilters();
+        options.CustomSchemaIds(x => x.FullName?.Replace("Dto", string.Empty));
+
+        // Set the comments path for the Swagger JSON and UI.
+        var xmlFiles = Directory.GetFiles(AppContext.BaseDirectory, "CookBook.Recipes.*.xml", SearchOption.TopDirectoryOnly).ToList();
+        xmlFiles.ForEach(xmlFile => options.IncludeXmlComments(xmlFile));
+    });
+
+services
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    })
+    .AddHttpExceptions(options =>
+    {
+        // Only log the when it has a status code of 500 or higher, or when it not is a HttpException.
+        options.ShouldLogException = exception =>
+        {
+            return (exception is HttpExceptionBase httpException &&
+                (int)httpException.StatusCode >= 500) ||
+                exception is not HttpExceptionBase;
+        };
+    });
+
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseHttpExceptions();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(options =>
+    {
+        options.RouteTemplate = ".less-known/api-docs/{documentName}.json";
+        options.PreSerializeFilters.Add((swagger, _) =>
+            // Clear servers -element in swagger.json because it got the wrong port when hosted behind reverse proxy
+            swagger.Servers.Clear());
+
+    });
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/.less-known/api-docs/v1.json", "v1");
+        options.RoutePrefix = ".less-known/api-docs/ui";
+    });
+}
+
+app
+    .AddEndpointFilter<OperationCancelledExceptionFilter>()
+    .WithOpenApi();
+
+app.Run();
+
