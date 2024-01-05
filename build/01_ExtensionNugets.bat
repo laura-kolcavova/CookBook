ea3f25bb47524d0cdf9fa8c0a2f89d8af97a81f0
Laura Kolčavová <53749720+laura-kolcavova@users.noreply.github.com>
2024-01-05 21:22:41 +0100

    feat: Add nuget packing bat files

---
+++ build/01_ExtensionNugets.bat
@@ -0,0 +1,9 @@
+cd ../extensions/CookBook.Extensions.Configuration/build
+call 01_PackNuget.bat
+cd ../../../build
+
+cd ../extensions/CookBook.Extensions.AspNetCore/build
+call 01_PackNuget.bat
+cd ../../../build
+
+
