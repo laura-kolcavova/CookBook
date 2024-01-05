3ba258b76ebb5cb3ec31b0a68aca196321caff63
Laura Kolčavová <laura.kolcavova@notino.com>
2024-01-05 19:52:39 +0100

    feat: update

--- CookBook.Recipes/src/CookBook.Recipes.Api/Program.cs
+++ CookBook.Recipes/src/CookBook.Recipes.Api/Program.cs
@@ -2,9 +2,8 @@ using CookBook.Extensions.AspNetCore.Filters;
 using CookBook.Extensions.Configuration.SqlServer;
 using CookBook.Recipes.Api.Configuration;
 using CookBook.Recipes.Api.Endpoints;
-using Microsoft.OpenApi.Models;
+using CookBook.Recipes.Api.Extensions;
 using Opw.HttpExceptions.AspNetCore;
-using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,60 +21,21 @@ var configuration = builder.Configuration;
 var cookBookRecipesConnectionString = configuration
     .GetSqlConnectionString(ConfigurationConstants.CookBookRecipesConnectionStringSectionName);
 
-//services
-//    .AddAuthentication(options =>
-//    {
-//        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-//        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-//    })
-//    .AddJwtBearer(options => builder.Configuration.GetSection(nameof(JwtBearerOptions)).Bind(options));
-
 services
     .AddOptions();
 
 services
-    .AddHealthChecks()
-    .AddSqlServer(cookBookRecipesConnectionString, name: "CookBookRecipes_DB", tags: new[] { "readiness" });
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-services
-    .AddEndpointsApiExplorer()
-    .AddSwaggerExamplesFromAssemblyOf<Program>()
-    .AddSwaggerGen(options =>
-    {
-        options.SwaggerDoc("v1", new OpenApiInfo { Title = builder.Environment.ApplicationName, Version = "v1" });
-        options.ExampleFilters();
-        options.CustomSchemaIds(x => x.FullName?.Replace("Dto", string.Empty));
-
-        // Set the comments path for the Swagger JSON and UI.
-        var xmlFiles = Directory.GetFiles(AppContext.BaseDirectory, "CookBook.Recipes.*.xml", SearchOption.TopDirectoryOnly).ToList();
-        xmlFiles.ForEach(xmlFile => options.IncludeXmlComments(xmlFile));
-    });
-
-//services
-//.AddJsonOptions(options =>
-//{
-//    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-//})
-//.AddHttpExceptions(options =>
-//{
-//    // Only log the when it has a status code of 500 or higher, or when it not is a HttpException.
-//    options.ShouldLogException = exception =>
-//    {
-//        return (exception is HttpExceptionBase httpException &&
-//            (int)httpException.StatusCode >= 500) ||
-//            exception is not HttpExceptionBase;
-//    };
-//});
-
+    .InstallDbServices(cookBookRecipesConnectionString)
+    .InstallSwaggerServices(builder.Environment.ApplicationName)
+    .InstallApiServices();
 
 var app = builder.Build();
 
 app.UseRouting();
 app.UseHttpExceptions();
 
-app.UseAuthentication();
-app.UseAuthorization();
+//app.UseAuthentication();
+//app.UseAuthorization();
 
 if (app.Environment.IsDevelopment())
 {
@@ -85,7 +45,6 @@ if (app.Environment.IsDevelopment())
         options.PreSerializeFilters.Add((swagger, _) =>
             // Clear servers -element in swagger.json because it got the wrong port when hosted behind reverse proxy
             swagger.Servers.Clear());
-
     });
 
     app.UseSwaggerUI(options =>
@@ -101,4 +60,3 @@ app
     .WithOpenApi();
 
 app.Run();
-
