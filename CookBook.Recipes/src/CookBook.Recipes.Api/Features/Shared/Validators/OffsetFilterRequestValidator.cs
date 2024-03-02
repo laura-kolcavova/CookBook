c01c7180175efb7e0a936221f34c5dbbec21463e
Laura Kolčavová <laura.kolcavova@gmail.com>
2024-03-02 09:31:33 +0100

    feat: Add recipe detail

---
+++ CookBook.Recipes/src/CookBook.Recipes.Api/Features/Shared/Validators/OffsetFilterRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using CookBook.Recipes.Api.Features.Shared.Dto;
+using FluentValidation;
+
+namespace CookBook.Recipes.Api.Features.Shared.Validators;
+
+internal sealed class OffsetFilterRequestValidator : AbstractValidator<OffsetFilterRequestDto>
+{
+    public OffsetFilterRequestValidator()
+    {
+        RuleFor(request => request.Offset)
+            .GreaterThanOrEqualTo(0);
+
+        RuleFor(request => request.Limit)
+            .GreaterThanOrEqualTo(0);
+    }
+}
