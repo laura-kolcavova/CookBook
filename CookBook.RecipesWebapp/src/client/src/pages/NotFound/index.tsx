4d25e4bde59308ca2cb6f4883b85e7698134d42d
Laura Kolčavová <laura.kolcavova@gmail.com>
2026-01-18 23:16:35 +0100

    feat: page not found

---
+++ CookBook.RecipesWebapp/src/client/src/pages/NotFound/index.tsx
@@ -0,0 +1,9 @@
+export const NotFound = () => {
+  return (
+    <div className="page-container mx-auto py-6">
+      <div className="py-24 text-center">
+        <span className="text-xl font-bold text-text-color-primary">PAGE NOT FOUND</span>
+      </div>
+    </div>
+  );
+};
