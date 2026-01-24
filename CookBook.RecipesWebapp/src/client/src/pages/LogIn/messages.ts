13b124e8c1af30523c1a94f9fe163d46c92ac252
Laura Kolčavová <laura.kolcavova@gmail.com>
2026-01-24 22:47:12 +0100

    feat: update

---
+++ CookBook.RecipesWebapp/src/client/src/pages/LogIn/messages.ts
@@ -0,0 +1,24 @@
+import { defineMessages } from 'react-intl';
+
+export const messages = defineMessages({
+  emailLabel: {
+    id: 'logIn.emailLabel',
+    defaultMessage: 'Email',
+  },
+  passwordLabel: {
+    id: 'logIn.passwordLabel',
+    defaultMessage: 'Password',
+  },
+  logInButton: {
+    id: 'logIn.logInButton',
+    defaultMessage: 'Log In',
+  },
+  needAccount: {
+    id: 'logIn.needAccount',
+    defaultMessage: 'Need an account?',
+  },
+  registerLink: {
+    id: 'logIn.registerLink',
+    defaultMessage: 'Register',
+  },
+});
--- CookBook.RecipesWebapp/src/client/src/pages/NotFound/index.tsx
+++ CookBook.RecipesWebapp/src/client/src/pages/NotFound/index.tsx
@@ -1,9 +1,14 @@
+import { FormattedMessage } from 'react-intl';
+import { messages } from './messages';
+
 export const NotFound = () => {
   return (
     <div className="bg-content-background-color-primary">
       <div className="container mx-auto py-10 px-4">
         <div className="py-24 text-center">
-          <span className="text-xl font-bold text-text-color-primary">Page not found</span>
+          <span className="text-xl font-bold text-text-color-primary">
+            <FormattedMessage {...messages.pageNotFound} />
+          </span>
         </div>
       </div>
     </div>
