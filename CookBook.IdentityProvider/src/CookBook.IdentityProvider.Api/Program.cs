8b0af156fb605cbc03726ebc0d25bb42c7ae6e07
Laura Kolčavová <laura.kolcavova@gmail.com>
2026-01-29 20:29:36 +0100

    feat: add identity provider

---
+++ CookBook.IdentityProvider/src/CookBook.IdentityProvider.Api/Program.cs
@@ -0,0 +1,52 @@
+using CookBook.IdentityProvider.Api.Shared.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+var configuration = builder.Configuration;
+var isDevelopment = builder.Environment.IsDevelopment();
+
+builder.Host
+    .UseDefaultServiceProvider((context, options) =>
+    {
+        options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+        options.ValidateOnBuild = context.HostingEnvironment.IsDevelopment();
+    });
+
+services
+    .AddOptions();
+
+services
+    .AddApi(
+        builder.Environment.ApplicationName);
+
+var app = builder.Build();
+
+if (isDevelopment)
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler();
+}
+
+app.UseRouting();
+
+app.UseSwagger(options =>
+{
+    options.RouteTemplate = ".less-known/api-docs/{documentName}.json";
+    options.PreSerializeFilters.Add((swagger, _) =>
+        // Clear servers -element in swagger.json because it got the wrong port when hosted behind reverse proxy
+        swagger.Servers.Clear());
+});
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/.less-known/api-docs/v1.json", "v1");
+    options.RoutePrefix = ".less-known/api-docs/ui";
+    options.ConfigObject.Filter = string.Empty;
+    options.ConfigObject.TryItOutEnabled = true;
+});
+
+app.Run();
---
+++ CookBook.IdentityProvider/src/CookBook.IdentityProvider.Api/Shared/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using CookBook.Extensions.AspNetCore.Shared;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text.Json.Serialization;
+
+namespace CookBook.IdentityProvider.Api.Shared.Extensions;
+
+internal static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApi(this IServiceCollection services, string applicationName)
+    {
+        services
+            .ConfigureHttpJsonOptions(options =>
+            {
+                options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+
+        services
+            .AddEndpointsApiExplorer()
+            .AddSwaggerExamplesFromAssemblyOf<Program>()
+            .AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = applicationName,
+                    Version = "v1"
+                });
+
+                options.ExampleFilters();
+                options.SupportNonNullableReferenceTypes();
+
+                options.CustomSchemaIds(x => x.FullName?
+                    .Replace("Dto", string.Empty)
+                    .Replace("+", "."));
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFiles = Directory
+                    .GetFiles(
+                        AppContext.BaseDirectory,
+                        "CookBook.IdentityProvider.*.xml",
+                        SearchOption.TopDirectoryOnly)
+                    .ToList();
+
+                xmlFiles.ForEach(xmlFile => options.IncludeXmlComments(xmlFile));
+            });
+
+        services
+            .AddProblemDetails()
+            .AddInternalOrPublicValidators(typeof(Program).Assembly, ServiceLifetime.Singleton);
+
+        return services;
+    }
+}
