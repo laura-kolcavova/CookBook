6d47848abd2fe0590e5c9ae8a07995ceae724d78
Laura Kolčavová <laura.kolcavova@gmail.com>
2026-02-21 16:19:01 +0100

    feat: update

--- CookBook.IdentityProvider/database/dbo/Tables/OpenIddictAuthorizations.sql
+++ CookBook.IdentityProvider/database/dbo/Tables/OpenIddictAuthorizations.sql
@@ -1,35 +1,41 @@
-USE [CookBookUsers]
-GO
-
-/****** Object:  Table [dbo].[OpenIddictAuthorizations]    Script Date: 2/21/2026 12:35:06 AM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE TABLE [dbo].[OpenIddictAuthorizations](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[ApplicationId] [int] NULL,
-	[ConcurrencyToken] [nvarchar](50) NULL,
-	[CreationDate] [datetime2](7) NULL,
-	[Properties] [nvarchar](max) NULL,
-	[Scopes] [nvarchar](max) NULL,
-	[Status] [nvarchar](50) NULL,
-	[Subject] [nvarchar](400) NULL,
-	[Type] [nvarchar](50) NULL,
- CONSTRAINT [PK_OpenIddictAuthorizations] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
+    [Id] [int] IDENTITY(1,1) NOT NULL,
+    [ApplicationId] [int] NULL,
+    [ConcurrencyToken] [nvarchar](50) NULL,
+    [CreationDate] [datetime2](7) NULL,
+    [Properties] [nvarchar](max) NULL,
+    [Scopes] [nvarchar](max) NULL,
+    [Status] [nvarchar](50) NULL,
+    [Subject] [nvarchar](400) NULL,
+    [Type] [nvarchar](50) NULL,
+    CONSTRAINT [PK_OpenIddictAuthorizations] PRIMARY KEY CLUSTERED (
+        [Id] ASC
+    )
+    WITH (
+        PAD_INDEX = OFF,
+        STATISTICS_NORECOMPUTE = OFF,
+        IGNORE_DUP_KEY = OFF,
+        ALLOW_ROW_LOCKS = ON,
+        ALLOW_PAGE_LOCKS = ON,
+        OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF)
+    ON [PRIMARY]
+)
+ON [PRIMARY] TEXTIMAGE_ON [PRIMARY];
 
-ALTER TABLE [dbo].[OpenIddictAuthorizations]  WITH CHECK ADD  CONSTRAINT [FK_OpenIddictAuthorizations_OpenIddictApplications_ApplicationId] FOREIGN KEY([ApplicationId])
-REFERENCES [dbo].[OpenIddictApplications] ([Id])
-GO
+GO;
 
-ALTER TABLE [dbo].[OpenIddictAuthorizations] CHECK CONSTRAINT [FK_OpenIddictAuthorizations_OpenIddictApplications_ApplicationId]
-GO
+ALTER TABLE [dbo].[OpenIddictAuthorizations]
+WITH CHECK ADD CONSTRAINT [FK_OpenIddictAuthorizations_OpenIddictApplications_ApplicationId]
+FOREIGN KEY(
+    [ApplicationId]
+)
+REFERENCES [dbo].[OpenIddictApplications] (
+    [Id]
+);
 
+GO;
 
+ALTER TABLE [dbo].[OpenIddictAuthorizations]
+CHECK CONSTRAINT [FK_OpenIddictAuthorizations_OpenIddictApplications_ApplicationId];
+
+GO;
--- CookBook.IdentityProvider/database/dbo/Tables/OpenIddictScopes.sql
+++ CookBook.IdentityProvider/database/dbo/Tables/OpenIddictScopes.sql
@@ -1,28 +1,26 @@
-USE [CookBookUsers]
-GO
-
-/****** Object:  Table [dbo].[OpenIddictScopes]    Script Date: 2/21/2026 12:35:39 AM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE TABLE [dbo].[OpenIddictScopes](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[ConcurrencyToken] [nvarchar](50) NULL,
-	[Description] [nvarchar](max) NULL,
-	[Descriptions] [nvarchar](max) NULL,
-	[DisplayName] [nvarchar](max) NULL,
-	[DisplayNames] [nvarchar](max) NULL,
-	[Name] [nvarchar](200) NULL,
-	[Properties] [nvarchar](max) NULL,
-	[Resources] [nvarchar](max) NULL,
- CONSTRAINT [PK_OpenIddictScopes] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
+    [Id] [int] IDENTITY(1,1) NOT NULL,
+    [ConcurrencyToken] [nvarchar](50) NULL,
+    [Description] [nvarchar](max) NULL,
+    [Descriptions] [nvarchar](max) NULL,
+    [DisplayName] [nvarchar](max) NULL,
+    [DisplayNames] [nvarchar](max) NULL,
+    [Name] [nvarchar](200) NULL,
+    [Properties] [nvarchar](max) NULL,
+    [Resources] [nvarchar](max) NULL,
 
+    CONSTRAINT [PK_OpenIddictScopes] PRIMARY KEY CLUSTERED (
+        [Id] ASC
+    )
+    WITH (
+        PAD_INDEX = OFF,
+        STATISTICS_NORECOMPUTE = OFF,
+        IGNORE_DUP_KEY = OFF,
+        ALLOW_ROW_LOCKS = ON,
+        ALLOW_PAGE_LOCKS = ON,
+        OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF)
+    ON [PRIMARY]
+)
+ON [PRIMARY] TEXTIMAGE_ON [PRIMARY];
 
+GO;
