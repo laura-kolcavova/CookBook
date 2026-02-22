0e6193b4129ee3834024ee3a5e9a0f8c208e1345
Laura Kolčavová <laura.kolcavova@gmail.com>
2026-02-22 20:05:40 +0100

    feat: update

--- CookBook.RecipesWebapp/server/src/CookBook.RecipesWebapp.Server.Infrastructure/Shared/OpenIddict/OpenIddictConfiguration.cs
+++ CookBook.RecipesWebapp/server/src/CookBook.RecipesWebapp.Server.Infrastructure/Shared/OpenIddict/OpenIddictConfiguration.cs
@@ -1,6 +1,9 @@
-﻿namespace CookBook.RecipesWebapp.Server.Infrastructure.Shared.OpenIddict;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CookBook.RecipesWebapp.Server.Infrastructure.Shared.OpenIddict;
 
 public sealed class OpenIddictConfiguration
 {
+    [Required]
     public string IssuerUri { get; init; } = string.Empty;
 }
