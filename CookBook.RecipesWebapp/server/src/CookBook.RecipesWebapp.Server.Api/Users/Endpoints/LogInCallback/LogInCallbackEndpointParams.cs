96e55ad45dd3400882147d0fe382e89e90de68d8
Laura Kolčavová <laura.kolcavova@gmail.com>
2026-03-09 00:43:13 +0100

    feat: update

---
+++ CookBook.RecipesWebapp/server/src/CookBook.RecipesWebapp.Server.Api/Users/Endpoints/LogInCallback/LogInCallbackEndpointParams.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CookBook.RecipesWebapp.Server.Api.Users.Endpoints.LogIn;
+
+internal sealed record LogInCallbackEndpointParams
+{
+    [FromRoute]
+    public string Provider { get; init; } = string.Empty;
+}
