10978be393d0ea059c686aedc855390feb49a8f0
Laura Kolčavová <laura.kolcavova@gmail.com>
2026-03-12 22:13:05 +0100

    fix: update

--- CookBook.RecipesWebapp/client/src/utils/csrfToken.ts
+++ CookBook.RecipesWebapp/client/src/utils/csrfToken.ts
@@ -1,9 +1,11 @@
 import { ANTIFORGERY_REQUEST_TOKEN_COOKIE_NAME } from '~/constants';
 
 export const getCsrfToken = (): string | undefined => {
+  const cookieKeyName = `${ANTIFORGERY_REQUEST_TOKEN_COOKIE_NAME}=`;
+
   const csrfToken = document.cookie
     .split('; ')
-    .find((row) => row.startsWith(ANTIFORGERY_REQUEST_TOKEN_COOKIE_NAME))
+    .find((row) => row.startsWith(cookieKeyName))
     ?.split('=')[1];
 
   return csrfToken;
