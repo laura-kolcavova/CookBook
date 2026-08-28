94eaf3a648cb6ddbf581b6ce144bc6298de8c48d
Laura Kolčavová <laura.kolcavova@gmail.com>
2026-08-28 21:56:25 +0200

    fix: update

--- CookBook.RecipesWebapp/server/src/CookBook.RecipesWebapp.Server.Infrastructure/Users/Services/CurrentUserProfileFetcher.cs
+++ CookBook.RecipesWebapp/server/src/CookBook.RecipesWebapp.Server.Infrastructure/Users/Services/CurrentUserProfileFetcher.cs
@@ -1,6 +1,7 @@
 using CookBook.RecipesWebapp.Server.Domain.Users.Models;
 using CookBook.RecipesWebapp.Server.Domain.Users.Services.Abstractions;
 using CookBook.RecipesWebapp.Server.Infrastructure.Shared.Proxy.IdentityProviderApi.Clients;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Refit;
 
@@ -8,12 +9,19 @@ namespace CookBook.RecipesWebapp.Server.Infrastructure.Users.Services;
 
 internal sealed class CurrentUserProfileFetcher(
     IIdentityProviderApiClient identityProviderApiClient,
+    IHttpContextAccessor httpContextAccessor,
     ILogger<CurrentUserProfileFetcher> logger) :
     ICurrentUserProfileFetcher
 {
     public async Task<CurrentUserProfileModel> FetchCurrentUserProfile(
         CancellationToken cancellationToken)
     {
+        if (httpContextAccessor.HttpContext is null)
+        {
+            throw new InvalidOperationException(
+                "FetchCurrentUserProfile represents the current user of an HTTP request and cannot be called outside of one, e.g. from a background job.");
+        }
+
         try
         {
             var response = await identityProviderApiClient.GetCurrentUserProfile(
